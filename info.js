216a194c5664edf4af3940ddf1d5d770ab800fa4
labulac <35835318+labulac@users.noreply.github.com>
2021-06-17 23:30:12 +0800

    Create info.js

---
+++ info.js
@@ -0,0 +1,10 @@
+[dir]
+yuan=D:/muban.xlsx
+xian=D:/sheet/
+
+[update]
+newversion=18
+downloadurl=https://cdn.jsdelivr.net/gh/labulac/qd@master/18.zip
+
+[yiyan]
+yiyan=https://v1.jinrishici.com/rensheng.txt
