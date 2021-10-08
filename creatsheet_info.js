a42fa21da1cacafaac152ef8e3530d6bdf64f6f1
labulac <740162752@qq.com>
2021-10-08 15:08:43 +0800

    aaa

--- creatsheet_info.js
+++ creatsheet_info.js
@@ -3,7 +3,7 @@ yuan=D:/muban.xlsx
 xian=D:/sheet/
 
 [update]
-newversion=27
-downloadurl=https://cdn.jsdelivr.net/gh/labulac/creatsheet_arrdio@main/dist/creatsheet.exe
+newversion=30
+downloadurl=https://ghproxy.com/https://raw.githubusercontent.com/labulac/creatsheet_arrdio/main/dist/creatsheet.exe
 [yiyan]
 yiyan=https://v1.jinrishici.com/rensheng.txt
